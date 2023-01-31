c60ad2e0bf58fb8b3d08465ebc13d6d38dde960a
Doroteya Donova <doroteia.donova@gmail.com>
2023-01-31 18:26:58 +0100

    script database ok

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,85 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET FOREIGN_KEY_CHECKS = 0;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+DROP TABLE IF EXISTS `dog`;
+
+CREATE TABLE `dog` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `breed` VARCHAR(100) NOT NULL,
+  `gender` VARCHAR(45) NOT NULL,
+  `age` VARCHAR(100) NOT NULL,
+  `birthday_date` VARCHAR(45) NULL,
+  `location` VARCHAR(200) NULL,
+  `status_adopted` VARCHAR(45) NULL,
+  `picture` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+INSERT INTO `dog`(name, breed, gender, age, birthday_date, location, status_adopted, picture) VALUES 
+  (
+    'Beliga',
+    'Setter anglais',
+    'Femelle',
+    'Junior',
+    '2021-02-02',
+    'Marseille',
+    'No',
+    'https://www.la-spa.fr/app/assets-spa/uploads/animals/69487/megane-69487-63d28de1acc61.jpg'
+  ),(
+    'Mars',
+    'American staffordshire',
+    'Mâle',
+    'Adulte',
+    '2017-03-06',
+    'Lyon',
+    'No',
+    'https://www.la-spa.fr/app/assets-spa/uploads/animals/58249/newton-58249-63405644a7304.jpg'
+  );
+
+DROP TABLE IF EXISTS `person` ;
+CREATE TABLE `person` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(200) NOT NULL,
+  `lastname` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(400) NOT NULL,
+  `phone_number` VARCHAR(45) NULL,
+  `password` VARCHAR(450) NULL,
+  `adress` VARCHAR(450) NULL,
+  `is_admin` TINYINT NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+INSERT INTO `person`(firstname, lastname, email, phone_number, password, adress, is_admin) VALUES 
+  (
+    'Doroteya',
+    'Donova',
+    'doroteya.donova@gmail.com',
+    '0782446798',
+    NULL,
+    '27 rue Pierre-Pertit 75000 Paris',
+    1
+  ), (
+    'Sophia',
+    'Rio',
+    'soph.rio@gmail.com',
+    '0689774368',
+    NULL,
+    '12 rue Saint-Blandine 69001 Lyon',
+    0
+  );
+
+
+DROP TABLE IF EXISTS `adoption` ;
+CREATE TABLE `adoption` (
+  `dog_id` INT NOT NULL,
+  `person_id` INT NOT NULL,
+  `adoption_date` DATE NOT NULL,
+  FOREIGN KEY (dog_id) REFERENCES dog(id),
+  FOREIGN KEY (person_id) REFERENCES person(id)
+)
+ENGINE = InnoDB;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="text-5xl text-center">
-      <Home />
-      <p className="text-yellow-500">coucou</p>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/adminpanel" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
